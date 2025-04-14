76cac9289b3de6f8c1950da59d6b3c7caaaaec50
Sophia Black <sophia@zwrks.com>
2025-04-14 14:50:55 +0100

    added missing comma

--- [gamemodes]/driftv/server/_security/main.lua
+++ [gamemodes]/driftv/server/_security/main.lua
@@ -23,7 +23,7 @@ Events = {
     setDriftPoint =uuid() .."driftV:SubmitDriftPoint",
     raceEnd = uuid() .."drift:EndRace",
     raceData = uuid() .."drift:GetRaceData",
-    sellVeh = uuid() .."drift:SellVehicle"
+    sellVeh = uuid() .."drift:SellVehicle",
 }
 
 
